a67c1dfce29e6107ea5b056c0a41f258a4d457a4
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-09 11:13:46 +0700

    membuat constructor untuk class node

--- serachtree.cpp
+++ serachtree.cpp
@@ -2,10 +2,18 @@
 #include<string>
 using namespace std;
 
-class node 
+class node
 {
 public:
     string info;
     node *leftchild;
     node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
