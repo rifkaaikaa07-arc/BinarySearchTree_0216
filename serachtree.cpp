742e26d113ce468b5fb306bbc537fc9275a000a9
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-09 11:17:11 +0700

    menambahkan kondisi pada void insert

--- serachtree.cpp
+++ serachtree.cpp
@@ -40,5 +40,20 @@ public:
         node *currentnode = NULL;
 
         search(element, parent, currentnode);
+
+        if (parent == NULL)
+        {
+            root = newnode;
+            return;
+        }
+
+        if(element < parent->info)
+        {
+            parent->leftchild = newnode;
+        }
+        else if(element > parent->info)
+        {
+            parent->rightchild = newnode;
+        }
     }
 };
