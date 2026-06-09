16d004b95bd34accf274b9b5f824e9775071cacb
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-09 11:18:22 +0700

    menambahkan isi untuk void search

--- serachtree.cpp
+++ serachtree.cpp
@@ -61,5 +61,17 @@ public:
     // as well as the current node of its parent
     void search(string element, node *&parent, node *&currentnode)
     {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
     }
 };
